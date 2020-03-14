b3454f8414eb5506675bd552b551366d01db397d
Alejandro Jimenez <alejandro@localhost.localdomain>
2020-03-14 18:37:05 -0400

    My First commit

--- src/app/pages/tab3/tab3.module.ts
+++ src/app/pages/tab3/tab3.module.ts
@@ -4,7 +4,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+// import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { ExploreContainerComponentModule } from '../../explore-container/explore-container.module';
 
 @NgModule({
   imports: [
